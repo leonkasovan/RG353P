0ef59b87c9a333937c1f38e16591c940cc6bc069
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2023-01-13 05:08:09 +0700

    Update setup_system.sh

--- setup_system.sh
+++ setup_system.sh
@@ -4,4 +4,3 @@ wget https://raw.githubusercontent.com/leonkasovan/RG353P/main/terminal_setup.sh
 wget https://raw.githubusercontent.com/leonkasovan/RG353P/main/multimedia_keys.conf -o /etc/triggerhappy/triggers.d/multimedia_keys.conf
 cp /etc/triggerhappy/triggers.d/multimedia_keys.conf /etc/triggerhappy/triggers.d/multimedia_keys_disabled.conf
 anbernic-save-overlay
-reboot
