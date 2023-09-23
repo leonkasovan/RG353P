748afe26016aa8556c9ab396681406a060a67ae3
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2023-09-24 04:13:06 +0700

    Update export_sysroot_for_dev.sh

--- recalbox/export_sysroot_for_dev.sh
+++ recalbox/export_sysroot_for_dev.sh
@@ -1,5 +1,7 @@
 cd /home/ark/recalbox-rpi4_64/output/host/aarch64-buildroot-linux-gnu/sysroot/
 mkdir -p usr/bin2 bin2 usr/libexec
+cp -a ../../share/cmake-3.22 usr/share
+cp -a ../include/ .
 cp -a ../../../host/bin/pkg* bin2
 cp -a ../../../host/bin/busybox bin2
 cp -a ../../lib/gcc usr/lib
@@ -9,16 +11,12 @@ cp -a ../../bin/aarch64-* usr/bin2
 cp -a ../../bin/toolchain-wrapper usr/bin2
 cp -a ../../bin/cmake usr/bin2
 cp -a /usr/bin/dirname usr/bin2
-ln -s lib lib64
-cd usr
-ln -s lib lib64
-cd ..
-
-7zr a sysroot_for_dev.7z usr/libexec usr/include/ usr/lib/ usr/bin2/ bin2/ lib/
-
+cp -a /usr/bin/make usr/bin2
+7zr a sysroot_for_dev.7z usr/libexec/gcc usr/include/ usr/lib/ usr/bin2/ usr/share/cmake-3.22 bin2/ lib/ include/
+rm -rf usr/share/cmake-3.22
+rm -rf include
 rm -rf usr/lib/gcc
 rm -rf usr/libexec
-rm -rf usr/bin2/dirname
 rm -rf usr/bin2
 rm -rf bin2
 mv sysroot_for_dev.7z ../../../../
