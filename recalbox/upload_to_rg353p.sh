9ea8fb344287bd9488fe0481015081f62472c4b6
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2023-09-23 22:46:23 +0700

    Update upload_to_rg353p.sh

--- recalbox/upload_to_rg353p.sh
+++ recalbox/upload_to_rg353p.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-aarch64-linux-gnu-strip -s emulationstation
+${HOME}/sysroot/usr/bin/aarch64-buildroot-linux-gnu-strip -s emulationstation
 7zr a emulationstation.7z emulationstation
 sftp root@192.168.1.7 <<EOF
 put emulationstation.7z
 EOF
+rm emulationstation.7z
+rm emulationstation
