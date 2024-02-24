e36ba2a3e428940b41b92e0932ab5f41aa1fa14d
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2024-02-24 20:42:14 +0700

    Update install_package_addon.sh

--- recalbox/install_package_addon.sh
+++ recalbox/install_package_addon.sh
@@ -9,19 +9,24 @@ fi
 
 wget https://github.com/leonkasovan/RG353P/releases/download/recalbox-9.1/recalbox-package-addon.7z
 7zr x -aoa -sdel recalbox-package-addon.7z
-
-echo "Installing File Manager..."
-mv addon/DinguxCommander_SDL2 /usr/bin/
-chmod a+x /usr/bin/DinguxCommander_SDL2
-
-echo "Installing Terminal..."
-mv addon/st /usr/bin/
-chmod a+x /usr/bin/st
+echo "============================"
 
 echo "Updating Addon SystemList..."
 mv addon/systemlist.xml /recalbox/share/system/.emulationstation/
 
-echo "Install SDL1.2 compatibility layer"
+echo "1. Installing File Manager..."
+mv addon/DinguxCommander_SDL2 /usr/bin/
+chmod a+x /usr/bin/DinguxCommander_SDL2
+
+echo "2. Installing Terminal..."
+mv addon/st /usr/bin/
+chmod a+x /usr/bin/st
+
+echo "3. Installing Joystick Tester..."
+mv addon/sdl_joystick_arm64 /usr/bin/
+chmod a+x /usr/bin/sdl_joystick_arm64
+
+echo "4. Install SDL1.2 compatibility layer..."
 mv addon/libSDL-1.2.so.1.2.69 /usr/lib/
 cd /usr/lib/
 rm libSDL.so
