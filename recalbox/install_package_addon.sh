bbecf2e531f3cff68953757e6d5f557ce96e3ab9
leonkasovan <110372618+leonkasovan@users.noreply.github.com>
2024-02-24 19:32:00 +0700

    Update install_package_addon.sh

--- recalbox/install_package_addon.sh
+++ recalbox/install_package_addon.sh
@@ -1,18 +1,30 @@
 #!/bin/sh
 
 mount -o remount,rw /
+
+cd /recalbox/share/roms
+if [ -d /recalbox/share/roms/addon/filecommander ]; then
+    rm -r /recalbox/share/roms/addon/filecommander
+fi
+
 wget https://github.com/leonkasovan/RG353P/releases/download/recalbox-9.1/recalbox-package-addon.7z
 7zr x recalbox-package-addon.7z
+
+echo "Installing File Manager..."
 mv addon/DinguxCommander_SDL2 /usr/bin/
 chmod a+x /usr/bin/DinguxCommander_SDL2
+
+echo "Installing Terminal..."
 mv addon/st /usr/bin/
 chmod a+x /usr/bin/st
-mv addon/File\ Commander.sh /recalbox/share/roms/addon/
-mv addon/Terminal.sh /recalbox/share/roms/addon/
-mv addon/filecommander /recalbox/share/roms/addon/
-mv addon/simpleterminal /recalbox/share/roms/addon/
-mv addon/systemlist.xml /recalbox/share/system/.emulationstation/
+
+echo "Install SDL1.2 compatibility layer"
 mv addon/libSDL-1.2.so.1.2.69 /usr/lib/
 cd /usr/lib/
+rm libSDL.so
+rm libSDL-1.2.so.0
 ln -s libSDL-1.2.so.1.2.69 libSDL.so
 ln -s libSDL-1.2.so.1.2.69 libSDL-1.2.so.0
+
+echo "Updating Addon SystemList..."
+mv addon/systemlist.xml /recalbox/share/system/.emulationstation/
